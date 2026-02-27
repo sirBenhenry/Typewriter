8411b2ec7e962eac7a9f0b70404da965299dace0
sirBenhenry <sirBenhenry@users.noreply.github.com>
2026-02-28 00:06:45 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+'''this is a symple typing trainer rnning in the terminal'''
+
+from colorama import init, Fore, Style, Cursor
+import readchar
+from readchar import key
+import sys
+
+
+def print_line(text,GREY,sentence_nr):
+    sys.stdout.write('\r\033[K')
+    sys.stdout.flush()
+    print(GREY + text[sentence_nr], end="\r")
+    sys.stdout.flush()
+
+def keyboard_input(text,RED,RESET, GREY, sentence_nr):
+    current = text[sentence_nr]
+    idx = 0
+    while idx < len(current):
+        i = current[idx]
+        k = readchar.readkey()
+
+        if i == '↵':
+            if k == key.ENTER:
+                exit
+            else:
+                continue
+
+        if k == key.BACKSPACE:
+            if idx > 0:
+                idx -= 1
+                print(Cursor.BACK(1), end="")
+                print(GREY + current[idx], end="")
+                print(Cursor.BACK(1), end="")
+                sys.stdout.flush()
+            continue
+
+        if k == i:
+            print(RESET + k, end="")
+            idx += 1
+        else:
+            if k == ' ':
+                print(RED + '_', end="")
+                idx += 1
+            else:
+                print(RED + k, end="")
+                idx += 1
+
+        sys.stdout.flush()
+
+
+def main():
+    GREEN = Fore.GREEN
+    RED = Fore.RED
+    GREY = Fore.LIGHTBLACK_EX
+    RESET = Style.RESET_ALL
+    sentence_nr = 0
+    text = ['Heute suche ich meine USB Sticks und drücke auf jede Taste.↵', 'I hope this shit works.↵']
+    for i in text:
+        print_line(text,GREY,sentence_nr)
+        keyboard_input(text,RED,RESET, GREY, sentence_nr)
+        sentence_nr += 1
+
+
+
+
+
+
+
+if __name__ == '__main__':
+     main()
