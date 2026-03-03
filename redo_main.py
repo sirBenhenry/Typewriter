e849d19abd7a01b586563a61c95ece66e15514d8
sirBenhenry <sirBenhenry@users.noreply.github.com>
2026-03-03 18:43:16 +0100

    added colours

--- redo_main.py
+++ redo_main.py
@@ -17,11 +17,14 @@ def main():
     idx = 0
     sentence_nr = 0
     typed = ''
-    text = ['Heute suche ich meine USB Sticks und drickest auf jede Taste.↵', 'I hope this shit works.↵']
+    text = ['Lorem Ipsum dolor sit Amet, consectetur Adipiscing elit.↵', 'Ut enim ad minim Veniam, quis nostrud Exercitation ullamco Laboris.↵']
+    current_line = '[bright_black italic]' + text[sentence_nr] + '[/bright_black italic]'
 
+    print_ui(mistakes, wpm, accuracy, time, current_line, used_os)
     while sentence_nr <= len(text) - 1:
         typed, sentence_nr, idx = input(text, sentence_nr, idx, typed)
         current_line = merge(text, sentence_nr, idx, typed)
+        mistakes = spellcheck(typed, sentence_nr, text, mistakes)
         print_ui(mistakes, wpm, accuracy, time, current_line, used_os)
 
 
@@ -36,7 +39,7 @@ def print_ui(mistakes, wpm, accuracy, time, current_line, used_os):
     clear_terminal(used_os)
     print(
         f"""
-        [red]Mistakes:[/red] {mistakes} | WPM: {wpm} | Accuracy: {accuracy} | Time: {time}
+        Mistakes: {mistakes} | WPM: {wpm} | Accuracy: {accuracy} | Time: {time}
         {current_line}
             """
     )
@@ -59,6 +62,7 @@ def input(text, sentence_nr, idx, typed):
             typed = ''
         else:
             return typed, sentence_nr, idx
+
     else:
         typed += k
         idx += 1
@@ -68,11 +72,23 @@ def input(text, sentence_nr, idx, typed):
 
 def merge(text, sentence_nr, idx, typed):
     sentence = text[sentence_nr]
-    current_line = '[bright_black italic]' + sentence[idx - 1 :] + '[/bright_black italic]'
+    current_line = '[bright_black italic]' + sentence[idx:] + '[/bright_black italic]'
     current_line = typed + current_line
-    current_line = current_line[:idx] 
+    current_line = current_line[:idx] + '[bright_cyan]|[/bright_cyan]' + current_line[idx:]
 
     return current_line
+
+
+def spellcheck(typed, sentence_nr, text, mistakes):
+    idx = 0
+    mistakes = 0
+    for i in typed:
+        if i == text[sentence_nr][idx]:
+            idx += 1
+        else:
+            mistakes += 1
+            idx += 1
+    return mistakes
 
 
 if __name__ == '__main__':
